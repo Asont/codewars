50d558c88c8b806c5e30a440418c3da4e3c4c2b0
Asont <94078125+Asont@users.noreply.github.com>
2021-12-03 00:52:30 +0300

    Add files via upload

---
+++ Basic Mathematical Operations.js
@@ -0,0 +1,20 @@
+function basicOp(operation, value1, value2)
+{
+  switch (operation) {
+      case "+": return value1+value2;
+      breake;
+      case "-": return value1-value2;
+      break;
+      case "*": return value1*value2;
+      break;
+      case "/": return value1/value2;
+      break;
+  }
+} 
+
+/*Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output */
