fb56a106b60e84c4e7140411cc695c96783ff584
Asont <94078125+Asont@users.noreply.github.com>
2021-12-03 12:55:09 +0300

    Add files via upload

---
+++ Sum The Strings.js
@@ -0,0 +1,8 @@
+function sumStr(a,b) {
+    let c = Number(a)+Number(b);
+     return c.toString();
+   }
+
+   /*Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+Example: (Input1, Input2 -->Output)*/
