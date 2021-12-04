1094c6f729781068e54184c0067999b72943e4ba
Asont <94078125+Asont@users.noreply.github.com>
2021-12-04 14:34:21 +0300

    Add files via upload

---
+++ Grasshopper - Function syntax debugging.js
@@ -0,0 +1,9 @@
+function main (verb, noun) {
+  let newStr = verb + noun;
+  return newStr;   
+}
+
+
+/*
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+*/
