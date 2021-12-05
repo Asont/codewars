9b7abe4360510e216bc07f627fe7cadccbafaabd
Asont <94078125+Asont@users.noreply.github.com>
2021-12-05 11:36:26 +0300

    Add files via upload

---
+++ Reversed Words.js
@@ -0,0 +1,10 @@
+function reverseWords(str){
+  let arr = str.split(" ").reverse();
+  let newStr = arr.join(" ");
+  return newStr; 
+}
+
+/*
+Complete the solution so that it reverses all of the words within the string passed in.
+
+*/
