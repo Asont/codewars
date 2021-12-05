8345bbe1f7e5c6acac4c68841d02c08a086f62dd
Asont <94078125+Asont@users.noreply.github.com>
2021-12-05 14:25:19 +0300

    Add files via upload

---
+++ Combine strings function.js
@@ -0,0 +1,7 @@
+function combineNames (str1, str2) {
+  return str1+" "+str2;
+}
+
+/*
+Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+*/
