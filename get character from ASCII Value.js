1e08d412e612567f0754bce90b651fcb084fd931
Asont <94078125+Asont@users.noreply.github.com>
2021-12-06 14:02:17 +0300

    Add files via upload

---
+++ get character from ASCII Value.js
@@ -0,0 +1,11 @@
+function getChar(c){
+  return String.fromCodePoint(c);
+}
+
+/*
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+get_char(65) # => 'A
+*/
