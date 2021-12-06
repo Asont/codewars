70d5c9556294d04f2f83ebd4df597dc0ed429cde
Asont <94078125+Asont@users.noreply.github.com>
2021-12-06 09:28:20 +0300

    Add files via upload

---
+++ Name Shuffle.js
@@ -0,0 +1,10 @@
+function nameShuffler(str){
+  let arr = str.split(" ");
+  arr.reverse();
+  return arr.join(" ");
+}
+
+/*
+Write a function that returns a string in which firstname is swapped with last name.
+
+*/
